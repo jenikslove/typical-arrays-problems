b3b5aee84f952f4e12353daecfb65f8ae1ef7015
test4334 <test4334@yandex.ru>
2020-09-21 09:56:56 +0300

    fix ruturn 0 if arr.length===0

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@
 exports.min = function min (array) {
   if (!array.length) {
   return Math.min.apply(null, array);
-}
+}else return 0;
 }
 
 exports.max = function max (array) {
   if (!array.length){
   return Math.max.apply(null, array);
-}
+}else return 0;
 }
 
 exports.avg = function avg (array) {
   if (!array.length) {
  return array.reduce((a, b) => (a + b)) / array.length;
-}
+}else return 0;
 }
 
 
