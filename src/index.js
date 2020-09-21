38aa67f39f53cedf0140000aad584ad96ae09e9f
test4334 <test4334@yandex.ru>
2020-09-21 10:04:34 +0300

    fix: syntax error if

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 
 exports.min = function min (array) {
-  if (!array.length) {
+  if (array.length) {
   return Math.min.apply(null, array);
 }else return 0;
 }
 
 exports.max = function max (array) {
-  if (!array.length){
+  if (array.length){
   return Math.max.apply(null, array);
 }else return 0;
 }
 
 exports.avg = function avg (array) {
-  if (!array.length) {
+  if (array.length) {
  return array.reduce((a, b) => (a + b)) / array.length;
 }else return 0;
 }
