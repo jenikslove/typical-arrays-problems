786fdbb2f608545874b92fdf4e2ee787d42f5ccc
test4334 <test4334@yandex.ru>
2020-09-21 09:54:52 +0300

    fix: func min,max,avg if array.length===0

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 
 exports.min = function min (array) {
+  if (!array.length) {
   return Math.min.apply(null, array);
+}
 }
 
 exports.max = function max (array) {
+  if (!array.length){
   return Math.max.apply(null, array);
+}
 }
 
 exports.avg = function avg (array) {
+  if (!array.length) {
  return array.reduce((a, b) => (a + b)) / array.length;
+}
 }
 
 
